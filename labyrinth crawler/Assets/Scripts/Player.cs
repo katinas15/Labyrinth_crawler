27ea632d747351088bbd06d31299519d85f6be16
katinas15 <laimonas.janutenas@gmail.com>
2019-11-16 13:28:46 +0200

    galima vaikscioti

---
+++ labyrinth crawler/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1f;
+
+    Rigidbody2D rigidBody;
+    BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = CrossPlatformInputManager.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var deltaY = CrossPlatformInputManager.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
