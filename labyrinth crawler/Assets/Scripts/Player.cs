56fb4b0612685b512a64d70137f43faa8df76128
katinas15 <laimonas.janutenas@gmail.com>
2019-11-16 21:30:56 +0200

    pakeisti sprite ir idetas animation

--- labyrinth crawler/Assets/Scripts/Player.cs
+++ labyrinth crawler/Assets/Scripts/Player.cs
@@ -9,11 +9,13 @@ public class Player : MonoBehaviour
 
     Rigidbody2D rigidBody;
     BoxCollider2D boxCollider;
+    Animator animator;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -27,6 +29,34 @@ public class Player : MonoBehaviour
         var newXPos = transform.position.x + deltaX;
         var deltaY = CrossPlatformInputManager.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed;
         var newYPos = transform.position.y + deltaY;
+
+        if (deltaY > 0f)
+        {
+            animator.SetBool("NormalWalk", false);
+            animator.SetBool("BackWalk", true);
+        }
+        else if (deltaY < 0f)
+        {
+            animator.SetBool("NormalWalk", true);
+            animator.SetBool("BackWalk", false);
+        } else if (deltaX != 0)
+        {
+            animator.SetBool("NormalWalk", true);
+            animator.SetBool("BackWalk", false);
+        } else
+        {
+            animator.SetBool("NormalWalk", false);
+            animator.SetBool("BackWalk", false);
+        }
+
+        if (deltaX < 0f)
+        {
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector2(1f, 1f);
+        }
 
         transform.position = new Vector2(newXPos, newYPos);
     }
